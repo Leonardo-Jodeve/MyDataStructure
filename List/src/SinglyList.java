49e49d8d320d333328758755dc9e7694a7fa0cf2
Leonardo-Jodeve <dszw2000@gmail.com>
2019-10-08 15:12:47 +0800

    my structure

---
+++ List/src/SinglyList.java
@@ -0,0 +1,114 @@
+public class SinglyList<T>
+{
+    public Node<T> head;
+
+    public SinglyList()
+    {
+        this.head=new Node<T>();
+    }
+
+    public SinglyList(T[] values)
+    {
+        this();
+        Node<T> rear=this.head;
+        for (int i = 0; i < values.length; i++)
+        {
+            T value = values[i];
+            rear.next = new Node<T>(value, null);
+            rear = rear.next;
+        }
+    }
+
+    public SinglyList(SinglyList<T> list)                   //深拷贝构造方法
+    {
+        this();
+        Node<T> rear=this.head;
+        for (Node<T> p=list.head.next;  p!=null;  p=p.next)
+        {
+            rear.next = new Node<T>(p.data, null);
+            rear = rear.next;
+        }
+    }
+
+    public boolean isEmpty()
+    {
+        return this.head.next==null;
+    }
+
+    public String toString()
+    {
+        String string=this.getClass().getName()+"(";
+        for(Node<T> p=this.head.next; p!=null; p=p.next)
+        {
+            string+=p.data.toString();
+            if(p.next!=null)
+                string+=",";
+        }
+        return string+")";
+    }
+
+    public T get(int i)
+    {
+        Node<T> p=this.head.next;
+        for(int j=0;p!=null&&j<i;j++)
+        {
+            p=p.next;
+        }
+        return (i>=0&&p!=null)?p.data:null;                 //判断i是否为负数，且指针p非空（tail.next==null）
+    }
+
+    public void insert(int i,T data)
+    {
+        Node<T> front=this.head;
+        for(int j=0;front!=null&&j<i;j++)
+            front=front.next;
+        front.next=new Node<T>(data,front.next);
+    }
+
+    public void addAll(int i, SinglyList<T> list)
+    {
+        if(list.head.next==null)
+            throw new NullPointerException();
+        SinglyList<T> copy=new SinglyList<T>(list);          //深拷贝不影响
+
+        Node<T> p=this.head,
+                tail=copy.head.next;                         //遍历this，找到插入点
+        for(int j=0;j<i&&p!=null;j++)
+            p=p.next;
+
+        while (tail.next!=null)                             //遍历list，找尾巴
+            tail=tail.next;
+        tail.next=p.next;
+        p.next=copy.head.next;
+    }
+
+    public boolean equals(Object obj)
+    {
+        if(obj instanceof SinglyList)
+        {
+            SinglyList compare=(SinglyList)obj;
+            Node<T> rare1=this.head.next,
+                    rare2=compare.head.next;
+            while (rare1!=null&&rare2!=null)
+            {
+                if(rare1.data.equals(rare2.data))
+                {
+                    rare1=rare1.next;
+                    rare2=rare2.next;
+                    continue;
+                }
+                return false;
+            }
+            if(rare1!=null||rare2!=null)
+                return false;
+            return true;
+        }
+        else
+        return false;
+    }
+
+    public SinglyList<T> subList(int i, int n)
+    {
+
+    }
+}
---
+++ List/src/SinglyList_main.java
@@ -0,0 +1,27 @@
+public class SinglyList_main
+{
+    public static void main(String[] args)
+    {
+        Integer[] numbers1={1,2,3,4,5},numbers2={6,7,8,9,10},
+        numbers3={1,2,3,4},numbers4={1,2,3,4,5},
+        numbers5={1,2,3,5},numbers6={1,2,3,4};
+
+        SinglyList<Integer> integerSinglyList1=new SinglyList<>(numbers1),
+        integerSinglyList2=new SinglyList<>(numbers2),
+        integerSinglyList3=new SinglyList<>(numbers3),
+        integerSinglyList4=new SinglyList<>(numbers4),
+        integerSinglyList5=new SinglyList<>(numbers5),
+                integerSinglyList6=new SinglyList<>(numbers6);
+
+        System.out.println("链表1源表是："+integerSinglyList1);
+        System.out.println("链表2源表是："+integerSinglyList2);
+        System.out.println("");
+        integerSinglyList1.addAll(1,integerSinglyList2);
+        System.out.println("链表1经过addAll(1,链表2)变为："+integerSinglyList1);
+        System.out.println("链表2保持不变："+integerSinglyList2+",深拷贝成功。");
+        System.out.println("");
+        System.out.println(integerSinglyList3.equals(integerSinglyList4));
+        System.out.println(integerSinglyList3.equals(integerSinglyList5));
+        System.out.println(integerSinglyList3.equals(integerSinglyList6));
+    }
+}
