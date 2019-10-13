27282c71697ed6eddbb1ff31e0ec7feca7bf3c12
Leonardo-Jodeve <dszw2000@gmail.com>
2019-10-13 20:06:12 +0800

    Sorted SinglyList

--- List/src/SortedSinglyList_main.java
+++ List/src/SortedSinglyList_main.java
@@ -3,10 +3,10 @@ public class SortedSinglyList_main
     public static void main(String[] args)
     {
         Integer[] numbers1={1,2,3,4,5,6};
-        SinglyList singlyList=new SinglyList(numbers1);
-        SortedSinglyList sortedSinglyList=new SortedSinglyList(singlyList,false);
+        SinglyList singlyList=new SinglyList<>(numbers1);
+        SortedSinglyList sortedSinglyList=new SortedSinglyList<>(singlyList,false);
         System.out.println("降序排列："+sortedSinglyList);
-        SortedSinglyList sortedSinglyList1=new SortedSinglyList(singlyList,true);
+        SortedSinglyList sortedSinglyList1=new SortedSinglyList<>(singlyList,true);
         System.out.println("升序排列："+sortedSinglyList1);
     }
 }
