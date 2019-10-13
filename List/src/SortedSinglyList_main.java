c6eca46f5a6d735c5f38cebe935cabbce207319e
Leonardo-Jodeve <dszw2000@gmail.com>
2019-10-13 20:00:22 +0800

    sorted Singltlist

--- List/src/SortedSinglyList_main.java
+++ List/src/SortedSinglyList_main.java
@@ -5,6 +5,8 @@ public class SortedSinglyList_main
         Integer[] numbers1={1,2,3,4,5,6};
         SinglyList singlyList=new SinglyList(numbers1);
         SortedSinglyList sortedSinglyList=new SortedSinglyList(singlyList,false);
-        System.out.println(sortedSinglyList);
+        System.out.println("降序排列："+sortedSinglyList);
+        SortedSinglyList sortedSinglyList1=new SortedSinglyList(singlyList,true);
+        System.out.println("升序排列："+sortedSinglyList1);
     }
 }
