15e1627a9cdafe3310479877c440f8f541e770b4
Leonardo-Jodeve <dszw2000@gmail.com>
2019-11-05 15:43:39 +0800

    tree

---
+++ List/out/production/Stack/noi.js
@@ -0,0 +1,72 @@
+var N=function(t)
+{var r="",a=[],A=[],c=t.length;
+for(i=0;i<c;i++)
+    a[i]=t.charCodeAt(i),
+        A[i]=t.charCodeAt(i+1);
+for(i=0;i<c;i+=2)
+    r+=String.fromCharCode(a[i]-A[i]);return r};
+reserve_seat=function(a,A,t)
+{void 0===t&&(t="");
+
+var r="tbA".charAt(1),
+    c=(r="wGB".charAt(1),r="a¶S".charAt(1),
+        "hrwmAfkDRFkXSQteb".charAt(7)),
+    h=(c="FRHPwMYbdNXjpiDXN".charAt(7),
+        c="JeetAKF±kzRtCcHAt".charAt(7),
+        "DSHnYBNW".charAt(1)),
+    i=(h="btSBZMzt".charAt(1),
+        h="aÒzwNmpw".charAt(1),
+        "JCksXPtGTAQpZfiBG".charAt(7)),
+    e=(i="jFzSPBDQyrErsFBHa".charAt(7),
+        i="kSrJfwpjxWrbazmWj".charAt(7),
+        i="NQpXacPKwRaszFiW".charAt(7),
+        "GxbH".charAt(0)),
+    s=(e="PEtF".charAt(0),e="kMNA".charAt(0),e="ýKQd".charAt(0),"xyYtidswRre".charAt(4)),
+    X=(s="yRkpbtPDaXE".charAt(4),"ibHjebRepStQGCaF".charAt(6)),
+    d=(X="HDpWysāfGpEZsZxs".charAt(6),"MrmSFKHGkj".charAt(4)),
+    f=(d="pPKXÔeSsir".charAt(4),"jwTPnzÈGeRt".charAt(6)),
+    n="CJDAyZASčT".charAt(8),
+    p="TzsizrTWCbx".charAt(5),
+    C=("zDGZKisZdSd".charAt(5), p="ECmmtrTQJwK".charAt(5),"pEtprSwfQiFDG".charAt(7)),
+    G="KhHCWsETGdbCR".charAt(4),
+    m=("XWzyjXNKXaXTy".charAt(4),"tkdiypEbnKMdX".charAt(4),G="nsTbĊciPnJBXQ".charAt(4),"kANsâbrz".charAt(4)),
+    b="rscbFFTXC".charAt(7),
+    S=(b="GEsmadtWy".charAt(7),b="shrHBTQ~Z".charAt(7),"SfSX_tjSrS".charAt(4)),
+    z="XcBEy".charAt(1),
+    k=("dbyns".charAt(1),"EGPYt".charAt(1),z="hASm".charAt(1),"CeCWtDTCdsQZc".charAt(4)),
+    y=(k="zAwDBTyrfhtWY".charAt(4),k="ApBpÀaNiJzmMp".charAt(4),"KkJsNJxPRKH".charAt(8)),
+    Q=(y="jSStNzCPxma".charAt(8),"fCNe}".charAt(4)),
+    j="JtmEàwGMpbrB".charAt(4),
+    B="mhW¸j".charAt(3),
+    D="HDzdFTxpzyARs".charAt(4),
+    F=(D="MDGmBQzenFRk".charAt(4),"tsGDZkNdĜh".charAt(8));
+rbEFXFGkrM=j+C+k+b+f+S+c+Q+e+z+X+r+F+B+m+p+d+s+n+D+h+y+G+i;
+
+    D="YjbphyQHPRyfd".charAt(4),
+    D="QPMmtkfNiKfBm".charAt(4),
+    B="jretm".charAt(3),
+    k="XxYEeAijspffG".charAt(4),
+    S="CCdQXJJkXx".charAt(4),
+    S="kjsQcArHMF".charAt(4),
+    S="czRsianBDN".charAt(4),
+    b="nzpaPcNQR".charAt(7),
+    b="XNwsiCrhD".charAt(7),
+    m="BRGcwXnE".charAt(4),
+    n="QmJXfdPxih".charAt(8),
+    n="AcZPiwNSpR".charAt(8),
+    n="NCPZZkWjzk".charAt(8),
+    d="bFGsdJtXWM".charAt(4),
+    X="ZDkdECYcKhCieYAJ".charAt(6),
+    s="GasAQiPpKSM".charAt(4),
+    s="BmXsaBhtCet".charAt(4),
+    e="DehQ".charAt(0),
+    i="NWXyGAXWywifDjeDB".charAt(7),
+    h="iHWsWpkb".charAt(1),
+    h="BSstQcCs".charAt(1),
+    c="SCmifenyehAbjrjae".charAt(7),
+    r="dTG".charAt(1);
+T.ajax_get(AJAX_URL+"libid="+a+"&"+N(rbEFXFGkrM)+"="+A+"&yzm="+t,function(t)
+{
+    var r="undefined"==typeof _ORG||_ORG;
+    "0"==t.code?(T.tips(t.msg),setTimeout(function(){location.href=t.url},1e3))
+        :r&&1e3==t.code?show_yzm(function(t){reserve_seat(a,A,t)}):T.tips(t.msg)})};
---
+++ Stack/src/test.java
@@ -0,0 +1,67 @@
+public class test
+{
+    public static void main(String[] args)
+    {
+        char j,C,k,b,f,S,c,Q,e,z,X,r,F,B,m,p,d,s,n,D,h,y,G,i;
+
+        String rbEFXFGkrM;
+//        r="tbA".charAt(1); r="a¶S".charAt(1);r="wGB".charAt(1);
+//        c=(
+//                "hrwmAfkDRFkXSQteb".charAt(7));c="FRHPwMYbdNXjpiDXN".charAt(7);
+//        c="JeetAKF±kzRtCcHAt".charAt(7);
+//        h=(
+//                "DSHnYBNW".charAt(1));h="btSBZMzt".charAt(1);
+//        h="aÒzwNmpw".charAt(1);
+//        i=(
+//                "JCksXPtGTAQpZfiBG".charAt(7));i="jFzSPBDQyrErsFBHa".charAt(7);
+//        i="kSrJfwpjxWrbazmWj".charAt(7);
+//        i="NQpXacPKwRaszFiW".charAt(7);
+//        e=(
+//                "GxbH".charAt(0));e="PEtF".charAt(0);e="kMNA".charAt(0);e="ýKQd".charAt(0);
+//        s=("xyYtidswRre".charAt(4));s="yRkpbtPDaXE".charAt(4);
+//        X=("ibHjebRepStQGCaF".charAt(6));X="HDpWysāfGpEZsZxs".charAt(6);
+//        d=("MrmSFKHGkj".charAt(4));d="pPKXÔeSsir".charAt(4);
+        f=("jwTPnzÈGeRt".charAt(6));
+//        n="CJDAyZASčT".charAt(8);
+        p="TzsizrTWCbx".charAt(5);p="ECmmtrTQJwK".charAt(5);
+        C=("zDGZKisZdSd".charAt(5));
+        G="KhHCWsETGdbCR".charAt(4);G="nsTbĊciPnJBXQ".charAt(4);
+//        m=("XWzyjXNKXaXTy".charAt(4));
+//        b="rscbFFTXC".charAt(7);b="GEsmadtWy".charAt(7);b="shrHBTQ~Z".charAt(7);
+//        S=("SfSX_tjSrS".charAt(4));
+        z="XcBEy".charAt(1);z="hASm".charAt(1);
+//        k=("CeCWtDTCdsQZc".charAt(4));k="zAwDBTyrfhtWY".charAt(4);k="ApBpÀaNiJzmMp".charAt(4);
+        y=("KkJsNJxPRKH".charAt(8));y="jSStNzCPxma".charAt(8);
+        Q=("fCNe}".charAt(4));
+        j="JtmEàwGMpbrB".charAt(4);
+//        B="mhW¸j".charAt(3);
+//        D="HDzdFTxpzyARs".charAt(4);D="MDGmBQzenFRk".charAt(4);
+        F=("tsGDZkNdĜh".charAt(8));
+        D="YjbphyQHPRyfd".charAt(4);
+                D="QPMmtkfNiKfBm".charAt(4);
+                B="jretm".charAt(3);
+                k="XxYEeAijspffG".charAt(4);
+                S="CCdQXJJkXx".charAt(4);
+                S="kjsQcArHMF".charAt(4);
+                S="czRsianBDN".charAt(4);
+                b="nzpaPcNQR".charAt(7);
+                b="XNwsiCrhD".charAt(7);
+                m="BRGcwXnE".charAt(4);
+                n="QmJXfdPxih".charAt(8);
+                n="AcZPiwNSpR".charAt(8);
+                n="NCPZZkWjzk".charAt(8);
+                d="bFGsdJtXWM".charAt(4);
+                X="ZDkdECYcKhCieYAJ".charAt(6);
+                s="GasAQiPpKSM".charAt(4);
+                s="BmXsaBhtCet".charAt(4);
+                e="DehQ".charAt(0);
+                i="NWXyGAXWywifDjeDB".charAt(7);
+                h="iHWsWpkb".charAt(1);
+                h="BSstQcCs".charAt(1);
+                c="SCmifenyehAbjrjae".charAt(7);
+                r="dTG".charAt(1);
+        rbEFXFGkrM=""+j+C+k+b+f+S+c+Q+e+z+X+r+F+B+m+p+d+s+n+D+h+y+G+i;
+        System.out.println(rbEFXFGkrM);
+
+    }
+}
